ddf98ad1f1e96d94754608596396446f6338fe69
JOSE <jmpcba@gmail.com>
2019-02-04 17:44:22 -0300

    agregar comentarios en prestadores

--- forms/ABM/frmPrestadores.Designer.vb
+++ forms/ABM/frmPrestadores.Designer.vb
@@ -55,12 +55,16 @@ Partial Class frmPrestadores
         Me.btnBuscar = New System.Windows.Forms.Button()
         Me.btnGuardar = New System.Windows.Forms.Button()
         Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.txtComentario = New System.Windows.Forms.TextBox()
+        Me.Label16 = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Panel1
         '
         Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.Label16)
+        Me.Panel1.Controls.Add(Me.txtComentario)
         Me.Panel1.Controls.Add(Me.txtServicio)
         Me.Panel1.Controls.Add(Me.Label15)
         Me.Panel1.Controls.Add(Me.cbEspecialidad)
@@ -91,12 +95,12 @@ Partial Class frmPrestadores
         Me.Panel1.Controls.Add(Me.Label2)
         Me.Panel1.Location = New System.Drawing.Point(4, 1)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(611, 437)
+        Me.Panel1.Size = New System.Drawing.Size(611, 492)
         Me.Panel1.TabIndex = 0
         '
         'txtServicio
         '
-        Me.txtServicio.Location = New System.Drawing.Point(97, 201)
+        Me.txtServicio.Location = New System.Drawing.Point(97, 131)
         Me.txtServicio.Name = "txtServicio"
         Me.txtServicio.Size = New System.Drawing.Size(164, 20)
         Me.txtServicio.TabIndex = 5
@@ -104,7 +108,7 @@ Partial Class frmPrestadores
         'Label15
         '
         Me.Label15.AutoSize = True
-        Me.Label15.Location = New System.Drawing.Point(7, 205)
+        Me.Label15.Location = New System.Drawing.Point(7, 135)
         Me.Label15.Name = "Label15"
         Me.Label15.Size = New System.Drawing.Size(57, 13)
         Me.Label15.TabIndex = 50
@@ -114,7 +118,7 @@ Partial Class frmPrestadores
         '
         Me.cbEspecialidad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cbEspecialidad.FormattingEnabled = True
-        Me.cbEspecialidad.Location = New System.Drawing.Point(97, 154)
+        Me.cbEspecialidad.Location = New System.Drawing.Point(97, 101)
         Me.cbEspecialidad.Name = "cbEspecialidad"
         Me.cbEspecialidad.Size = New System.Drawing.Size(220, 21)
         Me.cbEspecialidad.TabIndex = 3
@@ -122,7 +126,7 @@ Partial Class frmPrestadores
         'chbCese
         '
         Me.chbCese.AutoSize = True
-        Me.chbCese.Location = New System.Drawing.Point(54, 397)
+        Me.chbCese.Location = New System.Drawing.Point(53, 440)
         Me.chbCese.Name = "chbCese"
         Me.chbCese.Size = New System.Drawing.Size(36, 17)
         Me.chbCese.TabIndex = 49
@@ -132,7 +136,7 @@ Partial Class frmPrestadores
         'Label14
         '
         Me.Label14.AutoSize = True
-        Me.Label14.Location = New System.Drawing.Point(7, 378)
+        Me.Label14.Location = New System.Drawing.Point(6, 421)
         Me.Label14.Name = "Label14"
         Me.Label14.Size = New System.Drawing.Size(116, 13)
         Me.Label14.TabIndex = 48
@@ -140,21 +144,21 @@ Partial Class frmPrestadores
         '
         'dtCese
         '
-        Me.dtCese.Location = New System.Drawing.Point(386, 390)
+        Me.dtCese.Location = New System.Drawing.Point(385, 433)
         Me.dtCese.Name = "dtCese"
         Me.dtCese.Size = New System.Drawing.Size(200, 20)
         Me.dtCese.TabIndex = 47
         '
         'numFeriados
         '
-        Me.numFeriados.Location = New System.Drawing.Point(202, 346)
+        Me.numFeriados.Location = New System.Drawing.Point(201, 389)
         Me.numFeriados.Name = "numFeriados"
         Me.numFeriados.Size = New System.Drawing.Size(68, 20)
         Me.numFeriados.TabIndex = 7
         '
         'numDiferencial
         '
-        Me.numDiferencial.Location = New System.Drawing.Point(334, 346)
+        Me.numDiferencial.Location = New System.Drawing.Point(333, 389)
         Me.numDiferencial.Name = "numDiferencial"
         Me.numDiferencial.Size = New System.Drawing.Size(68, 20)
         Me.numDiferencial.TabIndex = 8
@@ -162,14 +166,14 @@ Partial Class frmPrestadores
         '
         'numFijo
         '
-        Me.numFijo.Location = New System.Drawing.Point(460, 346)
+        Me.numFijo.Location = New System.Drawing.Point(459, 389)
         Me.numFijo.Name = "numFijo"
         Me.numFijo.Size = New System.Drawing.Size(68, 20)
         Me.numFijo.TabIndex = 9
         '
         'numLunVie
         '
-        Me.numLunVie.Location = New System.Drawing.Point(70, 346)
+        Me.numLunVie.Location = New System.Drawing.Point(69, 389)
         Me.numLunVie.Name = "numLunVie"
         Me.numLunVie.Size = New System.Drawing.Size(68, 20)
         Me.numLunVie.TabIndex = 6
@@ -177,7 +181,7 @@ Partial Class frmPrestadores
         'Label13
         '
         Me.Label13.AutoSize = True
-        Me.Label13.Location = New System.Drawing.Point(199, 397)
+        Me.Label13.Location = New System.Drawing.Point(198, 440)
         Me.Label13.Name = "Label13"
         Me.Label13.Size = New System.Drawing.Size(165, 13)
         Me.Label13.TabIndex = 46
@@ -186,7 +190,7 @@ Partial Class frmPrestadores
         'Label12
         '
         Me.Label12.AutoSize = True
-        Me.Label12.Location = New System.Drawing.Point(457, 309)
+        Me.Label12.Location = New System.Drawing.Point(456, 352)
         Me.Label12.Name = "Label12"
         Me.Label12.Size = New System.Drawing.Size(72, 13)
         Me.Label12.TabIndex = 45
@@ -195,7 +199,7 @@ Partial Class frmPrestadores
         'Label11
         '
         Me.Label11.AutoSize = True
-        Me.Label11.Location = New System.Drawing.Point(331, 309)
+        Me.Label11.Location = New System.Drawing.Point(330, 352)
         Me.Label11.Name = "Label11"
         Me.Label11.Size = New System.Drawing.Size(77, 13)
         Me.Label11.TabIndex = 44
@@ -204,7 +208,7 @@ Partial Class frmPrestadores
         'Label10
         '
         Me.Label10.AutoSize = True
-        Me.Label10.Location = New System.Drawing.Point(199, 309)
+        Me.Label10.Location = New System.Drawing.Point(198, 352)
         Me.Label10.Name = "Label10"
         Me.Label10.Size = New System.Drawing.Size(90, 13)
         Me.Label10.TabIndex = 43
@@ -213,7 +217,7 @@ Partial Class frmPrestadores
         'Label9
         '
         Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(51, 309)
+        Me.Label9.Location = New System.Drawing.Point(50, 352)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(103, 13)
         Me.Label9.TabIndex = 42
@@ -222,7 +226,7 @@ Partial Class frmPrestadores
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(7, 286)
+        Me.Label8.Location = New System.Drawing.Point(7, 319)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(115, 13)
         Me.Label8.TabIndex = 37
@@ -230,7 +234,7 @@ Partial Class frmPrestadores
         '
         'txtEmail
         '
-        Me.txtEmail.Location = New System.Drawing.Point(97, 244)
+        Me.txtEmail.Location = New System.Drawing.Point(97, 157)
         Me.txtEmail.Name = "txtEmail"
         Me.txtEmail.Size = New System.Drawing.Size(358, 20)
         Me.txtEmail.TabIndex = 5
@@ -238,7 +242,7 @@ Partial Class frmPrestadores
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(7, 247)
+        Me.Label7.Location = New System.Drawing.Point(7, 160)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(39, 13)
         Me.Label7.TabIndex = 35
@@ -246,7 +250,7 @@ Partial Class frmPrestadores
         '
         'txtLocalidad
         '
-        Me.txtLocalidad.Location = New System.Drawing.Point(396, 155)
+        Me.txtLocalidad.Location = New System.Drawing.Point(396, 102)
         Me.txtLocalidad.Name = "txtLocalidad"
         Me.txtLocalidad.Size = New System.Drawing.Size(203, 20)
         Me.txtLocalidad.TabIndex = 4
@@ -254,7 +258,7 @@ Partial Class frmPrestadores
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(323, 157)
+        Me.Label6.Location = New System.Drawing.Point(323, 104)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(67, 13)
         Me.Label6.TabIndex = 33
@@ -262,7 +266,7 @@ Partial Class frmPrestadores
         '
         'txtApellido
         '
-        Me.txtApellido.Location = New System.Drawing.Point(97, 89)
+        Me.txtApellido.Location = New System.Drawing.Point(70, 75)
         Me.txtApellido.Name = "txtApellido"
         Me.txtApellido.Size = New System.Drawing.Size(203, 20)
         Me.txtApellido.TabIndex = 1
@@ -270,7 +274,7 @@ Partial Class frmPrestadores
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(31, 93)
+        Me.Label3.Location = New System.Drawing.Point(4, 79)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(59, 13)
         Me.Label3.TabIndex = 31
@@ -279,7 +283,7 @@ Partial Class frmPrestadores
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(319, 93)
+        Me.Label1.Location = New System.Drawing.Point(292, 79)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(54, 13)
         Me.Label1.TabIndex = 30
@@ -287,7 +291,7 @@ Partial Class frmPrestadores
         '
         'txtNombre
         '
-        Me.txtNombre.Location = New System.Drawing.Point(379, 89)
+        Me.txtNombre.Location = New System.Drawing.Point(352, 75)
         Me.txtNombre.Name = "txtNombre"
         Me.txtNombre.Size = New System.Drawing.Size(220, 20)
         Me.txtNombre.TabIndex = 2
@@ -295,7 +299,7 @@ Partial Class frmPrestadores
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(7, 162)
+        Me.Label5.Location = New System.Drawing.Point(7, 109)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(84, 13)
         Me.Label5.TabIndex = 27
@@ -333,7 +337,7 @@ Partial Class frmPrestadores
         Me.BtnCerrar.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.BtnCerrar.BackColor = System.Drawing.SystemColors.ActiveCaption
         Me.BtnCerrar.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.BtnCerrar.Location = New System.Drawing.Point(667, 259)
+        Me.BtnCerrar.Location = New System.Drawing.Point(667, 321)
         Me.BtnCerrar.Name = "BtnCerrar"
         Me.BtnCerrar.Size = New System.Drawing.Size(106, 33)
         Me.BtnCerrar.TabIndex = 203
@@ -343,7 +347,7 @@ Partial Class frmPrestadores
         'btnBuscar
         '
         Me.btnBuscar.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.btnBuscar.Location = New System.Drawing.Point(667, 70)
+        Me.btnBuscar.Location = New System.Drawing.Point(667, 132)
         Me.btnBuscar.Name = "btnBuscar"
         Me.btnBuscar.Size = New System.Drawing.Size(106, 33)
         Me.btnBuscar.TabIndex = 200
@@ -353,7 +357,7 @@ Partial Class frmPrestadores
         'btnGuardar
         '
         Me.btnGuardar.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.btnGuardar.Location = New System.Drawing.Point(667, 196)
+        Me.btnGuardar.Location = New System.Drawing.Point(667, 258)
         Me.btnGuardar.Name = "btnGuardar"
         Me.btnGuardar.Size = New System.Drawing.Size(106, 33)
         Me.btnGuardar.TabIndex = 202
@@ -363,19 +367,36 @@ Partial Class frmPrestadores
         'btnLimpiar
         '
         Me.btnLimpiar.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.btnLimpiar.Location = New System.Drawing.Point(667, 133)
+        Me.btnLimpiar.Location = New System.Drawing.Point(667, 195)
         Me.btnLimpiar.Name = "btnLimpiar"
         Me.btnLimpiar.Size = New System.Drawing.Size(106, 33)
         Me.btnLimpiar.TabIndex = 201
         Me.btnLimpiar.Text = "&LIMPIAR"
         Me.btnLimpiar.UseVisualStyleBackColor = True
         '
+        'txtComentario
+        '
+        Me.txtComentario.Location = New System.Drawing.Point(97, 190)
+        Me.txtComentario.Multiline = True
+        Me.txtComentario.Name = "txtComentario"
+        Me.txtComentario.Size = New System.Drawing.Size(413, 92)
+        Me.txtComentario.TabIndex = 51
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(7, 193)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(79, 13)
+        Me.Label16.TabIndex = 52
+        Me.Label16.Text = "COMENTARIO"
+        '
         'frmPrestadores
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.BtnCerrar
-        Me.ClientSize = New System.Drawing.Size(800, 497)
+        Me.ClientSize = New System.Drawing.Size(800, 559)
         Me.Controls.Add(Me.btnLimpiar)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.btnBuscar)
@@ -421,4 +442,6 @@ Partial Class frmPrestadores
     Friend WithEvents cbEspecialidad As ComboBox
     Friend WithEvents txtServicio As TextBox
     Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents txtComentario As TextBox
 End Class
