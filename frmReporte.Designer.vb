f6590da8ebf952e918ec8d5f9da02ec4d096fd74
Manuel <jmpcba@gmail.com>
2019-02-20 00:09:56 -0300

    format fechas y afiliado excel. llenado y formateo de grilla anual

--- frmReporte.Designer.vb
+++ frmReporte.Designer.vb
@@ -23,32 +23,58 @@ Partial Class frmReporte
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.dgAnual = New System.Windows.Forms.DataGridView()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripProgressBar1 = New System.Windows.Forms.ToolStripProgressBar()
         CType(Me.dgAnual, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'dgAnual
         '
         Me.dgAnual.AllowUserToAddRows = False
         Me.dgAnual.AllowUserToDeleteRows = False
+        Me.dgAnual.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.dgAnual.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.dgAnual.Location = New System.Drawing.Point(12, 67)
         Me.dgAnual.Name = "dgAnual"
         Me.dgAnual.ReadOnly = True
-        Me.dgAnual.Size = New System.Drawing.Size(643, 391)
+        Me.dgAnual.Size = New System.Drawing.Size(643, 377)
         Me.dgAnual.TabIndex = 0
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripProgressBar1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 455)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(675, 22)
+        Me.StatusStrip1.TabIndex = 1
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripProgressBar1
+        '
+        Me.ToolStripProgressBar1.Name = "ToolStripProgressBar1"
+        Me.ToolStripProgressBar1.Size = New System.Drawing.Size(100, 16)
         '
         'frmReporte
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(675, 470)
+        Me.ClientSize = New System.Drawing.Size(675, 477)
+        Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.dgAnual)
         Me.Name = "frmReporte"
         Me.Text = "frmReporte"
         CType(Me.dgAnual, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents dgAnual As DataGridView
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripProgressBar1 As ToolStripProgressBar
 End Class
--- frmReporte.vb
+++ frmReporte.vb
@@ -1,41 +1,70 @@
 ﻿Public Class frmReporte
     Dim dt As New DataTable
     Dim db As New DB
+    Dim ut As New utils
+
     Private Sub frmReporte_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        llenarGrilla()
+        ToolStripProgressBar1.Visible = False
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
+
+    Private Sub llenarGrilla()
         Dim DTFinal As New DataTable
-        dt = db.getLiquidacion(Today, DB.tiposLiquidacion.anual)
+        Try
+            dt = db.getLiquidacion(Today, DB.tiposLiquidacion.anual)
 
-        Dim datos = {"ID_PREST", "CUIT", "NOMBRE", "APELLIDO"}
+            Dim datos = {"ID_PREST", "CUIT", "NOMBRE", "APELLIDO"}
 
-        'AGREGAR COLUMNAS
-        For Each d In datos
-            DTFinal.Columns.Add(d)
-        Next
+            'AGREGAR COLUMNAS
+            For Each d In datos
+                DTFinal.Columns.Add(d)
+            Next
 
-        'AGREGAR MESES
-        For i = 1 To Today.Month
-            DTFinal.Columns.Add(MonthName(i).ToUpper)
-        Next
+            'AGREGAR MESES
+            For i = 1 To 12
+                DTFinal.Columns.Add(MonthName(i).ToUpper)
+            Next
 
-        For Each r As DataRow In dt.Rows
-            Dim idPrestador = r("ID_PREST")
-            Dim fecha = DateTime.Parse(r("MES"))
+            With ToolStripProgressBar1
+                .Visible = True
+                .Maximum = 0
+                .Maximum = dt.Rows.Count
+            End With
 
-            If DTFinal.Select("ID_PREST=" & idPrestador).Count = 0 Then
-                Dim newRow = DTFinal.NewRow
 
-                For Each d In datos
-                    newRow(d) = r(d)
-                Next
 
-                DTFinal.Rows.Add(newRow)
-            End If
+            For Each r As DataRow In dt.Rows
+                Dim idPrestador = r("ID_PREST")
+                Dim fecha = DateTime.Parse(r("MES"))
 
-            Dim row = DTFinal.Select("ID_PREST=" & idPrestador)
-            row(0)(MonthName(fecha.Month.ToString).ToUpper) = r("$ TOTAL")
-        Next
+                If DTFinal.Select("ID_PREST=" & idPrestador).Count = 0 Then
+                    Dim newRow = DTFinal.NewRow
 
-        dgAnual.DataSource = DTFinal
+                    For Each d In datos
+                        newRow(d) = r(d)
+                    Next
+
+                    DTFinal.Rows.Add(newRow)
+                End If
+
+                Dim row = DTFinal.Select("ID_PREST=" & idPrestador)
+                row(0)(MonthName(fecha.Month.ToString).ToUpper) = r("$ TOTAL")
+                ToolStripProgressBar1.Increment(1)
+            Next
+
+            With dgAnual
+                .DataSource = DTFinal
+                .Columns("ID_PREST").Visible = False
+                .AutoResizeColumns()
+                .AutoResizeRows()
+            End With
+
+        Catch ex As Exception
+            ut.mensaje(ex.Message, utils.mensajes.err)
+        Finally
+            ToolStripProgressBar1.Visible = False
+        End Try
     End Sub
 
     Private Sub frmReporte_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
